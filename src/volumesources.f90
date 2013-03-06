2174dd8a2aa97aa3ee156ceb4dcf4d1f5eaa5329
Vladimír Fuka <vladimir.fuka@mff.cuni.cz>
2013-03-06 10:57:08 +0100

    Reenabled scalar_flux_interface. Requires recently fixed GCC. Disabled some OpenMP and implemented actual resistance force computation.
    	modified:   volumesources.f90

--- src/volumesources.f90
+++ src/volumesources.f90
@@ -39,7 +39,7 @@ module VolumeSources
     procedure(resistance_interface),pointer  :: get_resistance       => null()
     procedure(resistance_interface),pointer  :: get_temperature_flux => null()
     procedure(resistance_interface),pointer  :: get_moisture_flux    => null()
-!     procedure(scalar_flux_interface),pointer :: get_scalar_flux      => null()
+    procedure(scalar_flux_interface),pointer :: get_scalar_flux      => null()
   end type TVolumeSourceBody
 
   abstract interface
@@ -49,12 +49,12 @@ module VolumeSources
       class(TVolumeSourceBody),intent(in) :: self
       real(KND),intent(in) :: x,y,z
     end function
-!     function scalar_flux_interface(self,x,y,z) result(res)
-!       import
-!       real(KND) :: res(computescalars) !HACK, allocatable causes ICE in gfortran 4.7.1
-!       class(TVolumeSourceBody),intent(in) :: self
-!       real(KND),intent(in) :: x,y,z
-!     end function
+    function scalar_flux_interface(self,x,y,z) result(res)
+      import
+      real(KND),allocatable :: res(:) !allocatable causes ICE in gfortran 4.7.1, ok in repository since March 2013
+      class(TVolumeSourceBody),intent(in) :: self
+      real(KND),intent(in) :: x,y,z
+    end function
   end interface
 
   type(TList) :: SourceBodiesList
@@ -98,18 +98,19 @@ module VolumeSources
           select type (PB)
             class is (TVolumeSourceBody)
               if (associated(PB%get_resistance)) then
-                !$omp parallel do private(i,j,k,elem)
                 do k = 0,Unz+1
                  do j = 0,Uny+1
                   do i = 0,Unx+1
-                     if (PB%Inside(xU(i),yPr(j),zPr(k))) then
+                     if (Inside(PB,xU(i),yPr(j),zPr(k))) then
+                       elem%xi = i
+                       elem%yj = j
+                       elem%zk = k
                        elem%resistance = PB%get_resistance(xU(i),yPr(j),zPr(k))
                        call UResistanceVolumesList%Add(elem)
                      end if
                   enddo
                  enddo
                 enddo
-                !$omp end parallel do
               end if
             class default
               stop "Not a TVolumeSourceBody."
@@ -124,18 +125,19 @@ module VolumeSources
           select type (PB)
             class is (TVolumeSourceBody)
               if (associated(PB%get_resistance)) then
-                !$omp parallel do private(i,j,k,elem)
                 do k = 0,Vnz+1
                  do j = 0,Vny+1
                   do i = 0,Vnx+1
-                     if (PB%Inside(xPr(i),yV(j),zPr(k))) then
+                     if (Inside(PB,xPr(i),yV(j),zPr(k))) then
+                       elem%xi = i
+                       elem%yj = j
+                       elem%zk = k
                        elem%resistance = PB%get_resistance(xPr(i),yV(j),zPr(k))
                        call VResistanceVolumesList%Add(elem)
                      end if
                   enddo
                  enddo
                 enddo
-                !$omp end parallel do
               end if
             class default
               stop "Not a TVolumeSourceBody."
@@ -150,18 +152,18 @@ module VolumeSources
           select type (PB)
             class is (TVolumeSourceBody)
               if (associated(PB%get_resistance)) then
-                !$omp parallel do private(i,j,k,elem)
                 do k = 0,Wnz+1
                  do j = 0,Wny+1
                   do i = 0,Wnx+1
-                     if (PB%Inside(xPr(i),yPr(j),zW(k))) then
-                       elem%resistance = PB%get_resistance(xPr(i),yPr(j),zW(k))
+                     if (Inside(PB,xPr(i),yPr(j),zW(k))) then
+                       elem%xi = i
+                       elem%yj = j
+                       elem%zk = k
                        call WResistanceVolumesList%Add(elem)
                      end if
                   enddo
                  enddo
                 enddo
-                !$omp end parallel do
               end if
             class default
               stop "Not a TVolumeSourceBody."
@@ -178,28 +180,35 @@ module VolumeSources
           select type (PB)
             class is (TVolumeSourceBody)
               allocate(Selem%scalar_flux(computescalars))  !to avoid allocation inside the loop
-              !$omp parallel do private(i,j,k,Telem,Melem,Selem)
               do k = 0,Prnz+1
                do j = 0,Prny+1
                 do i = 0,Prnx+1
-                   if (PB%Inside(xPr(i),yPr(j),zPr(k))) then
+                   if (Inside(PB,xPr(i),yPr(j),zPr(k))) then
                      if (associated(PB%get_temperature_flux)) then
+                       Telem%xi = i
+                       Telem%yj = j
+                       Telem%zk = k
                        Telem%temperature_flux = PB%get_temperature_flux(xPr(i),yPr(j),zPr(k))
                        call TemperatureFlVolumesList%Add(Telem)
                      end if
                      if (associated(PB%get_moisture_flux)) then
+                       Melem%xi = i
+                       Melem%yj = j
+                       Melem%zk = k
                        Melem%moisture_flux = PB%get_moisture_flux(xPr(i),yPr(j),zPr(k))
                        call MoistureFlVolumesList%Add(Melem)
                      end if
-!                      if (associated(PB%get_scalar_flux)) then
-!                        Selem%scalar_flux(:) = PB%get_scalar_flux(xPr(i),yPr(j),zPr(k)) !(:) avoiding reallocation
-!                        call ScalarFlVolumesList%Add(Selem)
-!                      end if
+                     if (associated(PB%get_scalar_flux)) then
+                       Selem%xi = i
+                       Selem%yj = j
+                       Selem%zk = k
+                       Selem%scalar_flux(:) = PB%get_scalar_flux(xPr(i),yPr(j),zPr(k)) !(:) avoiding reallocation
+                       call ScalarFlVolumesList%Add(Selem)
+                     end if
                    end if
                 enddo
                enddo
               enddo
-              !$omp end parallel do
             class default
               stop "Not a TVolumeSourceBody."
           end select
@@ -213,38 +222,38 @@ module VolumeSources
     ! as an argument, but we want the final arrays not polymorphic.
       integer :: i, component
 
-      component = 1
       allocate(UResistanceVolumes(UResistanceVolumesList%Len()))
-      component = 2
       allocate(VResistanceVolumes(VResistanceVolumesList%Len()))
-      component = 3
       allocate(WResistanceVolumes(WResistanceVolumesList%Len()))
       allocate(TemperatureFlVolumes(TemperatureFlVolumesList%Len()))
       allocate(MoistureFlVolumes(MoistureFlVolumesList%Len()))
       allocate(ScalarFlVolumes(ScalarFlVolumesList%Len()))
 
       i = 0
-      call UResistanceVolumesList%ForEach(CopyIBPoint)
+      component = 1
+      call UResistanceVolumesList%ForEach(CopyPoint)
       i = 0
-      call VResistanceVolumesList%ForEach(CopyIBPoint)
+      component = 2
+      call VResistanceVolumesList%ForEach(CopyPoint)
       i = 0
-      call WResistanceVolumesList%ForEach(CopyIBPoint)
+      component = 3
+      call WResistanceVolumesList%ForEach(CopyPoint)
       i = 0
-      call TemperatureFlVolumesList%ForEach(CopyIBPoint)
+      call TemperatureFlVolumesList%ForEach(CopyPoint)
       i = 0
-      call MoistureFlVolumesList%ForEach(CopyIBPoint)
+      call MoistureFlVolumesList%ForEach(CopyPoint)
       i = 0
-      call ScalarFlVolumesList%ForEach(CopyIBPoint)
+      call ScalarFlVolumesList%ForEach(CopyPoint)
 
       contains
 
-        subroutine CopyIBPoint(elem)
+        subroutine CopyPoint(elem)
           class(TListable) :: elem
 
           i = i + 1
 
           select type (elem)
-            type is (TResistanceVolume)
+            type is (TResistanceVolume)          
               if (component==1) then
                 UResistanceVolumes(i) = elem
               elseif (component==2) then
@@ -264,6 +273,69 @@ module VolumeSources
         end subroutine
 
     end subroutine MovePointsToArray
+
+
+
+    subroutine InitVolumeSources
+    
+      call InsideCellsToLists
+      call MovePointsToArray
+    end subroutine InitVolumeSources
+
+    
+
+    subroutine ResistanceForce(U2,V2,W2,U,V,W)
+      real(KND),dimension(-2:,-2:,-2:),intent(inout) :: U2,V2,W2
+      real(KND),dimension(-2:,-2:,-2:),intent(in)    :: U,V,W
+
+      call apply(U2,U,UResistanceVolumes,totU_u)
+      call apply(V2,V,VResistanceVolumes,totU_v)
+      call apply(W2,W,WResistanceVolumes,totU_w)
+
+      contains
+
+         pure real(KND) function totU_u(i,j,k)
+           integer,intent(in) :: i,j,k
+
+           totU_u = hypot( hypot( U(i,j,k), &
+                                  (V(i,j,k)+V(i,j-1,k)+V(i-1,j,k)+V(i-1,j-1,k))/4._KND ) , &
+                                  (W(i,j,k)+W(i,j,k-1)+W(i-1,j,k)+W(i-1,j,k-1))/4._KND )
+         end function
+
+         pure real(KND) function totU_v(i,j,k)
+           integer,intent(in) :: i,j,k
+
+           totU_v = hypot( hypot( (U(i,j,k)+U(i-1,j,k)+U(i,j-1,k)+U(i-1,j-1,k))/4._KND, &
+                                  V(i,j,k) ) , &
+                                  (W(i,j,k)+W(i,j,k-1)+W(i,j-1,k)+W(i,j-1,k-1))/4._KND )
+         end function
+
+         pure real(KND) function totU_w(i,j,k)
+           integer,intent(in) :: i,j,k
+
+           totU_w = hypot( hypot( (U(i,j,k)+U(i-1,j,k)+U(i,j,k-1)+U(i-1,j,k-1))/4._KND, &
+                                  (V(i,j,k)+V(i,j-1,k)+V(i,j,k-1)+V(i,j-1,k-1))/4._KND ) , &
+                                  W(i,j,k) )
+         end function
+
+         subroutine apply(X2,X,src,fun)
+           real(KND),dimension(-2:,-2:,-2:),intent(inout)  :: X2
+           real(KND),dimension(-2:,-2:,-2:),intent(in)     :: X
+           type(TResistanceVolume),allocatable,intent(in)  :: src(:)
+           procedure(totU_u) :: fun
+           integer i
+
+           if (allocated(src)) then
+             do i=1,size(src)
+               associate (xi => src(i)%xi, yj => src(i)%yj, zk => src(i)%zk)
+                 X2(xi,yj,zk) = X2(xi,yj,zk) - dt * src(i)%resistance * fun(xi,yj,zk) * X(xi,yj,zk)
+               end associate
+             end do
+           end if
+         end subroutine
+         
+    end subroutine ResistanceForce
+
 
 
 end module VolumeSources
