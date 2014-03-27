366a8b58933359d5feb4f749df66c963e3a69b02
Vladimír Fuka <vladimir.fuka@mff.cuni.cz>
2014-03-27 15:05:06 +0100

    New faster implementation. New generic procedure SWAPB available, swapping the bytes unconditionally.

--- src/endianness.f90
+++ src/endianness.f90
@@ -5,13 +5,19 @@ module Endianness
   implicit none
 
   private
-  public :: GetEndianness, BigEnd
+  public :: GetEndianness, BigEnd, SwapB
 
-  logical,save :: littleendian = .true.
+  logical, save :: littleendian = .true.
 
   interface BigEnd
+    module procedure BigEnd16
     module procedure BigEnd32
     module procedure BigEnd64
+  end interface
+
+  interface SwapB
+    module procedure SwapB32
+    module procedure SwapB64
   end interface
 
   contains
@@ -27,31 +33,102 @@ module Endianness
       endif
     end subroutine GetEndianness
 
+   
+    elemental function BigEnd16(x) result(res)
+      integer(int16) :: res
+      integer(int16),intent(in)::x
+      character(2) :: bytes
+      
+      if (.not.littleendian) then
+        res = x
+      else
+        bytes = transfer(x,bytes)
+        res = ichar(bytes(2:2),int16)
+        res = ior( ishft(ichar(bytes(1:1),int16),8), res )
+      endif
+    end function
+    
     elemental function BigEnd32(x) result(res)
       real(real32) :: res
-      real(real32),intent(in)::x
-      integer(int8),dimension(4):: bytes !may not work on some processors
-
+      real(real32),intent(in) :: x
+      character(4) :: bytes
+      integer(int32) :: t
+      
       if (.not.littleendian) then
         res = x
       else
-        bytes = transfer(x,bytes,4)
-        res = transfer(bytes(4:1:-1),res)
+        res = SwapB(x)
       endif
-    end function BigEnd32
-
+    end function
+    
     elemental function BigEnd64(x) result(res)
       real(real64) :: res
-      real(real64),intent(in)::x
-      integer(int8),dimension(8):: bytes !may not work on some processors
-
+      real(real64),intent(in) :: x
+      character(8) :: bytes
+      integer(int64) :: t
+      
       if (.not.littleendian) then
         res = x
       else
-        bytes = transfer(x,bytes,8)
-        res = transfer(bytes(8:1:-1),res)
+        res = SwapB(x)
       endif
-    end function BigEnd64
+    end function
+    
+    elemental function SwapB32(x) result(res)
+      real(real32) :: res
+      real(real32),intent(in) :: x
+      character(4) :: bytes
+      integer(int32) :: t
+      real(real32) :: rbytes, rt
+      !nicer looking TRANSFER is problematic to optimize by ifort
+      ! gfortran would be OK with that
+      equivalence (rbytes, bytes)
+      equivalence (t, rt)
+      
+      rbytes = x
+
+      t = ichar(bytes(4:4),int32)
+
+      t = ior( ishftc(ichar(bytes(3:3),int32),8),  t )
+
+      t = ior( ishftc(ichar(bytes(2:2),int32),16), t )
+
+      t = ior( ishftc(ichar(bytes(1:1),int32),24), t )
+
+      res = rt
+        
+    end function
+    
+    elemental function SwapB64(x) result(res)
+      real(real64) :: res
+      real(real64),intent(in) :: x
+      character(8) :: bytes
+      integer(int64) :: t
+      real(real64) :: rbytes, rt
+      equivalence (rbytes, bytes)
+      equivalence (t, rt)
+      
+      rbytes = x
+
+      t = ichar(bytes(8:8),int64)
+
+      t = ior( ishftc(ichar(bytes(7:7),int64),8),  t )
+
+      t = ior( ishftc(ichar(bytes(6:6),int64),16), t )
+
+      t = ior( ishftc(ichar(bytes(5:5),int64),24), t )
+
+      t = ior( ishftc(ichar(bytes(4:4),int64),32), t )
+
+      t = ior( ishftc(ichar(bytes(3:3),int64),40), t )
+
+      t = ior( ishftc(ichar(bytes(2:2),int64),48), t )
+
+      t = ior( ishftc(ichar(bytes(1:1),int64),56), t )
+
+      res = rt
+
+    end function
 
 end module Endianness
 
