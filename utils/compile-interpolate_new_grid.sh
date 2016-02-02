e0b2f4bcbe6d093b89692a91114ab5515cf12c15
Vladimir Fuka <vladimir.fuka@gmail.com>
2016-02-02 18:50:06 +0000

    Compilation script for interpolate_new_grid.

---
+++ utils/compile-interpolate_new_grid.sh
@@ -0,0 +1,11 @@
+gfortran -g -fbacktrace -cpp -Wall -Wno-maybe-uninitialized -Wno-unused-dummy-argument -fcheck=all \
+../src/lists.f90  \
+../src/strings.f90 \
+../src/stop.f90 \
+../src/parameters.f90 \
+../src/arrayutilities.f90 \
+../src/boundaries.f90 \
+../src/scalarboundaries.f90 \
+interpolate_new_grid.f90 \
+-o ~/bin/interpolate_new_grid \
+"$@"
