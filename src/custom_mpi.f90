02f5504899aa169ced9c3317d747a213c2e36b7c
Vladimír Fuka <vladimir.fuka@mff.cuni.cz>
2018-03-22 16:21:57 +0100

    Image numbers for plane and row communicators.

--- src/custom_mpi.f90
+++ src/custom_mpi.f90
@@ -149,6 +149,9 @@ module custom_par
   
   integer :: comm_plane_yz = MPI_COMM_NULL, comm_plane_xz = MPI_COMM_NULL, comm_plane_xy = MPI_COMM_NULL
   integer :: comm_row_x = MPI_COMM_NULL, comm_row_y = MPI_COMM_NULL, comm_row_z = MPI_COMM_NULL
+  integer :: my_plane_yz_im, my_plane_xz_im, my_plane_xy_im, my_row_x_im, my_row_y_im, my_row_z_im
+  
+  
   integer :: cart_comm_dim = -1
   integer, allocatable :: images_grid(:,:,:), ranks_grid(:,:,:)
   
@@ -387,11 +390,18 @@ contains
     call MPI_Comm_size(world_comm, world_comm_size, ie)
     if (ie/=0) call error_stop("Error calling MPI_Comm_size")
     
+    my_world_im = par_this_image(world_comm)
+    my_world_rank = my_world_im - 1
     
+
   contains
 
     type(c_ptr) function my_loc(t)
       integer, intent(in), target :: t
+      
+      !This is not standard conforming.
+      !We assume the address here is the same as the address
+      ! that MPI_IN_PLACE uses when passed as an argument.
       my_loc = c_loc(t)
     end function
       
@@ -825,13 +835,6 @@ contains
     myrank = myim - 1
 
     master = (myim == master_im)
-
-    my_world_im = par_this_image(world_comm)
-
-    my_world_rank = my_world_im - 1
-    
-
-!     npxyz = [npx, npy, npz]
     
     if (any(npxyz<1)) then
       npxyz(1) = 1
@@ -1022,6 +1025,14 @@ contains
   
     call MPI_Cart_sub(cart_comm, [.true., .false., .false.], comm_row_z, ie)
     if (ie/=0) call error_stop("Error creating comm_plane_x.")
+    
+    my_plane_yz_im = par_this_image(comm_plane_yz)
+    my_plane_xz_im = par_this_image(comm_plane_xz)
+    my_plane_xy_im = par_this_image(comm_plane_xy)
+
+    my_row_x_im = par_this_image(comm_row_x)
+    my_row_y_im = par_this_image(comm_row_y)
+    my_row_z_im = par_this_image(comm_row_z)
 
   end subroutine
   
