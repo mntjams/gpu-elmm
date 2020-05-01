dcd27a74b0e39a2bf2689f7326d5cda21584ae59
Vladimir Fuka <vladimir.fuka@mff.cuni.cz>
2020-05-01 16:16:53 +0200

    The parser in vtk_subset compatible with the output of Paraview. More input sanitizations and possibility to omit the extent if keeping it in full.

--- utils/vtk_subset.f90
+++ utils/vtk_subset.f90
@@ -260,10 +260,7 @@ contains
   subroutine read_header(g)
     use Endianness
     class(grid),intent(inout) :: g
-    integer :: io,nx,ny,nz
-    character(5) :: ch5
-    character(70) :: str
-    character :: ch
+    integer :: io
 
     open(newunit=g%unit,file=g%fname,access="stream",form="unformatted",status="old",action="read",iostat=io)
 
@@ -274,38 +271,126 @@ contains
       return
     end if
 
-    read(g%unit,pos=162,iostat=io) ch5
-    if (io/=0) call error
-
-    read(ch5,'(i5)') g%nx
-    nx=g%nx
+    call skip_to("X_COORDINATES", io)
+    if (io/=0) then
+      write(*,*) "Error, cannot find X_COORDINATES in '",g%fname,"'"
+      stop
+    end if
+    call get_number(g%nx)
+    call skip_line
+    
     allocate(g%x(g%nx))
-    read(g%unit,pos=219,iostat=io) g%x
+    read(g%unit,iostat=io) g%x
     g%x=BigEnd(g%x)
 
-    read(g%unit,pos=234+nx*4,iostat=io) ch5
-    read(ch5,'(i5)') g%ny
-    ny=g%ny
-
+    call skip_to("Y_COORDINATES", io)
+    if (io/=0) then
+      write(*,*) "Error, cannot find Y_COORDINATES in '",g%fname,"'"
+      stop
+    end if
+    call get_number(g%ny)
+    call skip_line
+    
     allocate(g%y(g%ny))
-    read(g%unit,pos=291+nx*4,iostat=io) g%y
+    read(g%unit,iostat=io) g%y
     g%y=BigEnd(g%y)
 
-    read(g%unit,pos=306+nx*4+ny*4,iostat=io) ch5
-    read(ch5,'(i5)') g%nz
-    nz=g%nz
+    call skip_to("Z_COORDINATES", io)
+    if (io/=0) then
+      write(*,*) "Error, cannot find Z_COORDINATES in '",g%fname,"'"
+      stop
+    end if
+    call get_number(g%nz)
+    call skip_line
+    
     allocate(g%z(g%nz))
-    read(g%unit,pos=363+nx*4+ny*4,iostat=io) g%z
+    read(g%unit,iostat=io) g%z
     g%z=BigEnd(g%z)
-    read(g%unit) ch,str
-    read(g%unit) ch
-    return
+    
+    call skip_line
+    call skip_line
     
   contains
-    subroutine error()
-      write(*,*) "Error reading from ",trim(g%fname)
-      stop 2
-    end subroutine    
+    subroutine skip_line
+      character :: ch
+      do
+        read(g%unit) ch
+        if (ch==new_line("a")) return
+      end do
+    end subroutine
+
+    subroutine get_number(n)
+      integer, intent(out) :: n
+      character :: ch
+      integer :: io
+      character(:), allocatable :: num_str
+      
+      do
+        read(g%unit, iostat=io) ch
+        if (io/=0) then
+          write(*,*) "Error reading from '",g%fname,"'."
+          stop 3
+        end if
+        if (ch/=' ') exit
+      end do
+      
+      num_str=ch
+      do
+        read(g%unit) ch
+        if (ch<'0' .or. ch>'9') exit
+        num_str = num_str // ch
+      end do
+
+      read(num_str,*) n
+    end subroutine
+
+    subroutine skip_to(str, stat)
+      character(*), intent(in) :: str
+      integer, intent(out) :: stat
+      character :: ch
+      integer :: io
+
+      do
+        read(g%unit, iostat=io) ch
+
+        if (io/=0) then
+          stat = 1
+          return
+        end if
+
+        if (ch==str(1:1)) then
+          call check(str(2:), stat)
+          if (stat == 0) return
+        end if
+
+      end do
+    end subroutine
+    
+    subroutine check(str, stat)
+      character(*), intent(in) :: str
+      integer, intent(out) :: stat
+      character :: ch
+      integer :: i, io
+
+      stat = 1
+      i = 0
+
+      do
+        i = i + 1
+
+        read(g%unit, iostat=io) ch
+
+        if (io/=0) return
+
+        if (ch/=str(i:i)) return
+
+        if (i==len(str)) then
+          stat = 0
+          return
+        end if
+      end do
+    end subroutine
+   
   end subroutine
   
   subroutine read_title(g,status,title)
@@ -315,58 +400,95 @@ contains
     character(:),allocatable,intent(out) :: title
     character(7) :: vtype
     character :: ch
-    integer :: io, n
+    integer :: io, n, lf_pos
     character(256) :: msg
+  
+    do
+      vtype=""
+      read(g%unit,iostat=io,iomsg=msg) vtype
+      
+      if (io==iostat_end) then
+        status = 0
+        return
+      else if (io/=0) then
+        write(*,'(*(g0))') io, trim(msg), "  ", g%fname, " ", "'",vtype,"'"
+        stop "Error reading title."
+      end if
+      
+      if (vtype=='VECTORS') then
+      
+        status = VECTOR
+        title = vtype
+        do
+          read(g%unit) ch
+          title = title // ch
+          if (ch==lf) exit
+        end do
+        
+        exit
+        
+      else if (vtype=='SCALARS') then
+      
+        status = SCALAR
+        title = vtype
+        n = 0
+        do
+          read(g%unit) ch
+          title = title // ch
+          if (ch==lf) n = n + 1
+          if (n==2) exit
+        end do
+        
+        exit
+        
+      else
+        !if our vtype buffer contains the end-of-line LF character, move back just behind it
+        !otherwise skip to the nearest one
+        lf_pos = scan(vtype, achar(10),.true.)
+        if (lf_pos>0) then
+          call stream_back(len(vtype)-lf_pos+1)
+        else
+          call skip_line
+        end if
+        
+      end if
+    end do
     
-    vtype=""
-    read(g%unit,iostat=io,iomsg=msg) vtype
-    
-    if (io==iostat_end) then
-      status = 0
-      return
-    else if (io/=0) then
-      write(*,'(*(g0))') io, trim(msg), "  ", g%fname, " ", "'",vtype,"'"
-      stop "Error reading title."
-    end if
-    
-    if (vtype=='VECTORS') then
-      status = VECTOR
-      title = vtype
+  contains
+  
+    subroutine skip_line
+      character :: ch
       do
         read(g%unit) ch
-        title = title // ch
-        if (ch==lf) exit
+        if (ch==new_line("a")) return
       end do
-    else if (vtype=='SCALARS') then
-      status = SCALAR
-      title = vtype
-      n = 0
-      do
-        read(g%unit) ch
-        title = title // ch
-        if (ch==lf) n = n + 1
-        if (n==2) exit
-      end do
-    else
-      write(*,'(*(g0))') "'",vtype,"'"
-      stop "Error reading title."
-    end if
+    end subroutine
+    
+    subroutine stream_back(n)
+      integer, intent(in) :: n
+      integer :: pos
+      character :: dummy
+      inquire(unit=g%unit,pos=pos)
+      read(g%unit,pos=pos-n) dummy
+    end subroutine
+
   end subroutine
   
   subroutine read_scalar(g,buf)
     class(grid),intent(in) :: g
     real(rp),intent(out) :: buf(:,:,:)
     character :: ch
-
+    
     read(g%unit) buf(1:g%nx,1:g%ny,1:g%nz)
     read(g%unit) ch
   end subroutine
   
   subroutine read_vector(g,buf)
+  use Endianness
     class(grid),intent(in) :: g
     real(rp),intent(out) :: buf(:,:,:,:)
     character :: ch
-    
+
     read(g%unit) buf(:,1:g%nx,1:g%ny,1:g%nz)
     read(g%unit) ch
   end subroutine
@@ -392,18 +514,18 @@ program vtk_subset
   character(:), allocatable :: title
   integer :: status, var_type, arg_len, title_pos
   
-  character(:), allocatable :: in_name, out_name, extent, fields
+  character(:), allocatable :: in_name, out_name, arg
   
   character(256), allocatable :: fields_arr(:)
   
-  integer :: i1, i2, j1, j2, k1, k2, ifield
+  integer :: i1=1, i2=1, j1=1, j2=1, k1=1, k2=1, ifield, iarg
   
   type(grid) :: in_g
   
   call GetEndianness
 
   if (command_argument_count()<2) then
-    write(*,*) "Usage: vtk_subset in_name out_name extent [field]"
+    write(*,*) "Usage: vtk_subset in_name out_name [extent] [field]"
     stop 1
   end if
  
@@ -414,36 +536,93 @@ program vtk_subset
   call get_command_argument(2, length=arg_len)
   allocate(character(arg_len) :: out_name)
   call get_command_argument(2, value=out_name)
-
-  call get_command_argument(3, length=arg_len)
-  allocate(character(arg_len) :: extent)
-  call get_command_argument(3, value=extent)
-  read(extent,*) i1,i2,j1,j2,k1,k2
   
-  nx = i2 - i1 + 1
-  ny = j2 - j1 + 1
-  nz = k2 - k1 + 1
+  nx = -1
+  ny = -1
+  nz = -1  
+  allocate(fields_arr(0))
+  
+  do iarg = 3,4
+    if (command_argument_count() >= iarg)  then
 
-  if (command_argument_count()==4) then
-    call get_command_argument(4, length=arg_len)
-    allocate(character(arg_len) :: fields)
-    call get_command_argument(4, value=fields)
-    fields_arr = split(fields,",")
-  else
-    allocate(fields_arr(0))
+      call get_command_argument(iarg, length=arg_len)
+      if (allocated(arg)) deallocate(arg)
+      allocate(character(arg_len) :: arg)
+      call get_command_argument(iarg, value=arg)
+      
+      if (arg(1:1)>='0' .and. arg(1:1)<='9') then
+        call parse_extent(arg)
+      else
+        call parse_fields(arg)
+      end if
+    end if
+  end do
+
+  if (i1<1 .or. j1<1 .or. k1<1) then 
+    write(*,*) "Error, one of the extents starts below 1."
+    write(*,*) "x extent start:", i1
+    write(*,*) "y extent start:", j1
+    write(*,*) "z extent start:", k1
+    stop 1
+  end if
+  
+  if (i1>i2 .or. j1>j2 .or. k1>k2) then 
+    write(*,*) "Error, one of the extents is negative."
+    write(*,*) "x extent:", i1, i2
+    write(*,*) "y extent:", j1, j2
+    write(*,*) "z extent:", k1, k2
+    stop 1
   end if
   
   in_g%fname = in_name
   call in_g%read_header
   
-
   nx_orig = in_g%nx
   ny_orig = in_g%ny
   nz_orig = in_g%nz
   
   if (nx_orig<=0 .or. ny_orig<=0 .or. nz_orig<=0) then
     write(*,*) "Error: '"//in_name//"' does not exist or is empty."
-    stop
+    stop 1
+  end if
+  
+  if (nx<0) then
+    nx = nx_orig
+    i1 = 1
+    i2 = nx
+  end if
+  
+  if (ny<0) then
+    ny = ny_orig
+    j1 = 1
+    j2 = ny
+  end if
+  
+  if (nz<0) then
+    nz = nz_orig
+    k1 = 1
+    k2 = nz
+  end if
+  
+  if (i2>nx_orig) then
+    write(*,*) "Error, requested x extent out of range."
+    write(*,*) "requested: ", i1, i2
+    write(*,*) "input file:", 1, nx_orig
+    stop 1
+  end if
+  
+  if (j2>ny_orig) then
+    write(*,*) "Error, requested y extent out of range."
+    write(*,*) "requested: ", j1, j2
+    write(*,*) "input file:", 1, ny_orig
+    stop 1
+  end if
+  
+  if (k2>nz_orig) then
+    write(*,*) "Error, requested z extent out of range."
+    write(*,*) "requested: ", k1, k2
+    write(*,*) "input file:", 1, nz_orig
+    stop 1
   end if
   
   allocate(sc(1:nx_orig,1:ny_orig,1:nz_orig))
@@ -475,6 +654,29 @@ program vtk_subset
   close(out_unit)
 
 contains
+
+  subroutine parse_extent(extent)
+    character(*), intent(in) :: extent
+    integer :: io
+    
+    read(extent,*, iostat=io) i1,i2,j1,j2,k1,k2
+    
+    if (io/=0) then
+      write(*,*) "Eror parsing the extent from '",trim(extent),"'."
+      stop 1
+    end if
+    
+    nx = i2 - i1 + 1
+    ny = j2 - j1 + 1
+    nz = k2 - k1 + 1    
+  end subroutine
+  
+  
+  subroutine parse_fields(fields)
+    character(*), intent(in) :: fields
+    
+    fields_arr = split(fields,",")
+  end subroutine
 
 
   subroutine get_buffer(status,sc,vec)
