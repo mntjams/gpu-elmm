649be0a0094b353da0d28005c08c127b20618482
Vladimir Fuka <vladimir.fuka@mff.cuni.cz>
2021-06-17 23:45:40 +0200

    A bounding box to analyze only an xyz subset of the eaf files, option to change the order of axes when constructing the data matrix.

--- utils/read_eaf.py
+++ utils/read_eaf.py
@@ -8,9 +8,14 @@ vector_flag = 1002
 
 class eaf_reader:
   
-    def __init__(self, base_name):
+    def __init__(self, base_name, bounding_box=None):
     
         self.base_name = base_name
+        
+        self.bounding_box = bounding_box
+        if self.bounding_box is not None:
+            self.bounding_box = np.asarray(self.bounding_box)
+        
 
         self.efh_name = base_name + ".efh"
 
@@ -19,8 +24,8 @@ class eaf_reader:
         one = np.fromfile(efh, dtype="int32", count=1)[0]
 
         if (one != 1):
-          print("Error, the data has the the opposite endianness!")
-          raise ValueError
+            print("Error, the data has the the opposite endianness!")
+            raise ValueError
             
         
         float_size = np.fromfile(efh, dtype="int32", count=1)[0]
@@ -34,10 +39,60 @@ class eaf_reader:
             raise ValueError
           
         self.nxyz = np.fromfile(efh, dtype="int32", count=3).tolist()
+        self.orig_nxyz = self.nxyz
+
         
         self.xs = np.fromfile(efh, dtype = self.dtype, count = self.nxyz[0])
         self.ys = np.fromfile(efh, dtype = self.dtype, count = self.nxyz[1])
         self.zs = np.fromfile(efh, dtype = self.dtype, count = self.nxyz[2])
+        
+        if self.bounding_box is not None:
+            for i in range(self.xs.size):
+                self.mini = self.xs.size
+                if self.xs[i] >= self.bounding_box[0]:
+                   self.mini = i
+                   break
+            for j in range(self.ys.size):
+                self.minj = self.ys.size
+                if self.ys[j] >= self.bounding_box[2]:
+                   self.minj = j
+                   break
+            for k in range(self.zs.size):
+                self.mink = self.zs.size
+                if self.zs[k] >= self.bounding_box[4]:
+                   self.mink = k
+                   break
+                    
+            for i in range(self.xs.size-1,-1,-1):
+                self.maxi = 0
+                if self.xs[i] <= self.bounding_box[1]:
+                   self.maxi = i+1
+                   break
+            for j in range(self.ys.size-1,-1,-1):
+                self.maxj = 0
+                if self.ys[j] <= self.bounding_box[3]:
+                   self.maxj = j+1
+                   break
+            for k in range(self.zs.size-1,-1,-1):
+                self.maxk = 0
+                if self.zs[k] <= self.bounding_box[5]:
+                   self.maxk = k+1
+                   break
+                 
+            self.nxyz = [self.maxi-self.mini,
+                         self.maxj-self.minj,
+                         self.maxk-self.mink]
+            self.xs = self.xs[self.mini:self.maxi]
+            self.ys = self.ys[self.minj:self.maxj]
+            self.zs = self.zs[self.mink:self.maxk]
+                    
+        else:
+            self.mini = 0
+            self.maxi = self.xs.size #intentionally +1
+            self.minj = 0
+            self.maxj = self.ys.size
+            self.mink = 0
+            self.maxk = self.zs.size
         
         self.arrays = []
         #Now for simplicity just assume that we know that the file contains velocity vectors only.
@@ -45,6 +100,8 @@ class eaf_reader:
         self.arrays = self.arrays + [{"name":"u", "vector":True}]
         
         self.transformed_xy = False
+        
+        
         
     def read_frame(self, n_frame):
         eaf = open(self.base_name + "-" + str(n_frame) + ".eaf", "rb")
@@ -54,11 +111,23 @@ class eaf_reader:
         for array in self.arrays:
             if array["vector"]:
               array_data = np.fromfile(eaf, dtype=self.dtype).reshape([3]+self.orig_nxyz, order='F')
+              
+              array_data = array_data[:,
+                                      self.mini:self.maxi,
+                                      self.minj:self.maxj,
+                                      self.mink:self.maxk]
+              
               if self.transformed_xy:
                 array_data = array_data.swapaxes(1,2)
                 array_data[[0,1],:,:,:] = array_data[[1,0],:,:,:]            
+
             else:
               array_data = np.fromfile(eaf, dtype=self.dtype).reshape(self.orig_nxyz, order='F')
+              
+              array_data = array_data[self.mini:self.maxi,
+                                      self.minj:self.maxj,
+                                      self.mink:self.maxk]
+              
               if self.transformed_xy:
                 array_data = array_data.swapaxes(0,1)
               
@@ -80,11 +149,13 @@ class eaf_reader:
     
     #converts the vector components u,v,w in data to one long 1D sequence
     #for certain specialized purposes, ignore if not needed
-    def step_data_matrix(self, data, name):
+    def step_data_matrix(self, data, name, swap=False):
         n = np.product(self.nxyz)
         for i in range(3*n):
           if data[0]["name"] == name:
             u = data[0]["data"]
+            if swap is not None:
+                u = u.swapaxes(swap[0],swap[1])
             res = np.ndarray([3*n])
             res[0:n] = u[0,:,:,:].reshape([n])
             res[n:2*n] = u[1,:,:,:].reshape([n])
@@ -93,17 +164,16 @@ class eaf_reader:
         return None    
      
     #returns a big array with a row of step_data_matrix times each time step
-    def steps_data_matrix(self, name, first, last, step):
+    def steps_data_matrix(self, name, first, last, step=1, swap=False):
         n = np.product(self.nxyz)
         nsteps = (last - first + 1) // step
         res = np.ndarray([3*n, nsteps])
+        
         k = 0
-        print(first, last+1, step)
-        print([k for k in range(first, last+1, step)])
         for tstep in range(first, last+1, step):
           print(tstep)
           data = self.read_frame(tstep)
-          res[:,k] = self.step_data_matrix(data, name)
+          res[:,k] = self.step_data_matrix(data, name, swap)
           k += 1
         return res
           
@@ -111,11 +181,53 @@ class eaf_reader:
     #switch x and y (and u and v)
     #useful when simulationg a channel in the y direction
     def transform_xy(self):
-        self.x, self.y = self.y, self.x
-        self.orig_nxyz = self.nxyz
+        self.xs, self.ys = self.ys, self.xs
         self.nxyz = np.asarray([self.nxyz[1],self.nxyz[0],self.nxyz[2]])
         self.transformed_xy = True
         
+        #if self.bounding_box is not None:
+            #self.bounding_box[[1,0]] = self.bounding_box[[0,1]]
+            
+            #for i in range(self.xs.size):
+                #self.mini = self.xs.size
+                #if self.xs[i] >= self.bounding_box[0]:
+                   #self.mini = i
+                   #break
+            #for j in range(self.ys.size):
+                #self.minj = self.ys.size
+                #if self.ys[j] >= self.bounding_box[2]:
+                   #self.minj = j
+                   #break
+            #for k in range(self.zs.size):
+                #self.mink = self.zs.size
+                #if self.zs[k] >= self.bounding_box[4]:
+                   #self.mink = k
+                   #break
+                    
+            #for i in range(self.xs.size-1,-1,-1):
+                #self.maxi = 0
+                #if self.xs[i] <= self.bounding_box[1]:
+                   #self.maxi = i+1
+                   #break
+            #for j in range(self.ys.size-1,-1,-1):
+                #self.maxj = 0
+                #if self.ys[j] <= self.bounding_box[3]:
+                   #self.maxj = j+1
+                   #break
+            #for k in range(self.zs.size-1,-1,-1):
+                #self.maxk = 0
+                #if self.zs[k] <= self.bounding_box[5]:
+                   #self.maxk = k+1
+                   #break
+                    
+        #else:
+            #self.mini = 0
+            #self.maxi = self.xs.size #intentionally +1
+            #self.minj = 0
+            #self.maxj = self.ys.size
+            #self.mink = 0
+            #self.maxk = self.zs.size
+       
       
                            
 if __name__ == '__main__':
